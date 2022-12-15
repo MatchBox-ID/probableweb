41c1b5115451caa77ada2470265217fcc646034f
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-12-15 21:30:18 +0700

    Create index.js

---
+++ Waproto/index.js
@@ -0,0 +1,29 @@
+//const inject.js
+const require buttonjs ['matchbox.proto.js']
+const require 'message'
+const require 'button'
+const require 'list-menu'
+const bubble if true
+//---------------------------\\
+send.messageInject const 'self';'true'
+EphemeralMessage.js
+${pushname} welcome message
+${prefix} command bot
+//---------------------------\\
+//PESAN DARI BOT KE USER
+if 'menu'
+{message:'${prefix}menu',
+{msgBot:'hello ${pushname} it is a interactive chatbot message','menu.js',
+{footerTeks:"Matchbox Probableweb"
+{index1:buttonjs, title:"Owner", type:'.owner',
+{index2:buttonjs, title:"github account", type:'.github'}
+
+if 'pools'
+{message:'${prefix}pool'}
+{msgbot:}
+{"
+           const type:pool.message
+             const title1:"question 1", vote.msg
+             const title2:"question 2", vote.msg
+             const title3:"question 3", vote.msg
+"}
