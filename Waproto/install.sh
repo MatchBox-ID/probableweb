5ad6c884eae4fcb21bec2e742475f1f5c9192d04
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-12-18 10:52:27 +0700

    Create install.sh

---
+++ Waproto/install.sh
@@ -0,0 +1,6 @@
+request if 'npm python3 install.sh'
+ if true echo 'probableweb anda akan diinstal'//// /-10/////-20////-30////-40////-50////-60////-70////-80////-90////-100
+  request if 'support-echo'
+installation
+Colorama
+Definition
